e750082bed98b3e2a17e862c18f4fd93420b766c
MonadoCycloneB <100519242+MonadoCycloneB@users.noreply.github.com>
2023-08-02 14:06:05 +0300

    Fix Zipping Script

--- zip.py
+++ zip.py
@@ -1,21 +1,26 @@
 import os
 import zipfile
 
-SRC_FOLDER = "src" # Project files directory
-ZIP_NAME = "ex.zip" # Name of the generated zip file
+SRC_FOLDER = "src"  # Project files directory
+ZIP_NAME = "ex2.zip"  # Name of the generated zip file
+
 
 def zip():
     try:
-        with zipfile.ZipFile(ZIP_NAME, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        zip_filepath = os.path.join(current_dir, ZIP_NAME)
+
+        with zipfile.ZipFile(zip_filepath, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for foldername, subfolders, filenames in os.walk(SRC_FOLDER):
                 for filename in filenames:
                     filepath = os.path.join(foldername, filename)
                     arcname = os.path.relpath(filepath, SRC_FOLDER)
                     zipf.write(filepath, arcname)
-        print(f"Successfully created '{ZIP_NAME}' containing all files from '{SRC_FOLDER}'.")
+        print(
+            f"Successfully created '{ZIP_NAME}' in '{current_dir}' containing all files from '{SRC_FOLDER}'.")
     except Exception as e:
         print(f"Error: {e}")
-    
+
 
 if __name__ == "__main__":
     zip()
