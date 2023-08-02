55e5ef376516525383c4c815ee2be24664dba237
MonadoCycloneB <100519242+MonadoCycloneB@users.noreply.github.com>
2023-08-02 14:13:09 +0300

    Another Fix For zip.py

--- zip.py
+++ zip.py
@@ -2,7 +2,7 @@ import os
 import zipfile
 
 SRC_FOLDER = "src"  # Project files directory
-ZIP_NAME = "ex2.zip"  # Name of the generated zip file
+ZIP_NAME = "ex.zip"  # Name of the generated zip file
 
 
 def zip():
@@ -12,15 +12,15 @@ def zip():
 
         with zipfile.ZipFile(zip_filepath, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for foldername, subfolders, filenames in os.walk(SRC_FOLDER):
+                if "__pycache__" in foldername:
+                    continue
                 for filename in filenames:
                     filepath = os.path.join(foldername, filename)
                     arcname = os.path.relpath(filepath, SRC_FOLDER)
                     zipf.write(filepath, arcname)
-        print(
-            f"Successfully created '{ZIP_NAME}' in '{current_dir}' containing all files from '{SRC_FOLDER}'.")
+        print(f"Successfully created '{ZIP_NAME}' in '{current_dir}' containing all files from '{SRC_FOLDER}'.")
     except Exception as e:
         print(f"Error: {e}")
-
 
 if __name__ == "__main__":
     zip()
