1b32221af4c113716fe930d6db2283a74f79459a
MonadoCycloneB <100519242+MonadoCycloneB@users.noreply.github.com>
2023-08-05 18:58:48 +0300

    Initial Commit

--- src/ex3.py
+++ src/ex3.py
@@ -2,12 +2,9 @@
 # FILE : ex3.py
 # WRITER : Dolev Irron , dolev.irron , 314635350
 # EXERCISE : intro2cse ex3 2023
-# DESCRIPTION: <INSERT DESCRIPTION>
-# STUDENTS I DISCUSSED THE EXERCISE WITH: Bugs Bunny, b_bunny.
-#								 	      Daffy Duck, duck_daffy.
-# WEB PAGES I USED: www.looneytunes.com/lola_bunny
-# NOTES: ...
+# DESCRIPTION: A file containing implementations of all exercise
+# no. 3 functions.
 #################################################################
 
 if __name__ == "__main__" :
-
+    pass
